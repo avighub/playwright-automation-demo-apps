877171f2297a469357e925487f5afc3366c9441f
Avishek Behera <avishekbehera87@gmail.com>
2024-04-03 20:21:42 +0530

    (tests): add tests for Login

--- tests/sample-login-tests/sample-login-app.spec.ts
+++ tests/sample-login-tests/sample-login-app.spec.ts
@@ -4,7 +4,7 @@ test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000");
 });
 
-test.describe("Check for Successful login", () => {
+test.describe("Auth Check", () => {
   test("should be able to login with valid credentials", async ({ page }) => {
     await expect(page.getByRole("heading", { name: "Login" })).toBeVisible();
 
@@ -13,8 +13,22 @@ test.describe("Check for Successful login", () => {
     await page.locator('input[name="password"]').type("password1");
     await page.getByRole("button", { name: "Login" }).click();
 
-    await expect(
-      page.getByRole("heading", { name: `Welcome ${email}` })
-    ).toBeVisible();
+    await expect(page.locator("id=welcomeMessage")).toHaveText(
+      `Welcome ${email}`
+    );
+    await expect(page.getByRole("link", { name: "Logout" })).toBeVisible();
+  });
+
+  test("should display validation error for invalid credentials", async ({
+    page,
+  }) => {
+    await expect(page.getByRole("heading", { name: "Login" })).toBeVisible();
+
+    const email = "user1@example.com";
+    await page.locator('input[name="email"]').type(email);
+    await page.locator('input[name="password"]').type("invalidpassword");
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.locator("id=message")).toHaveText("Invalid Credentials");
   });
 });
